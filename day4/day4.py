c596e2271abe945f80d21b9472bf66d5835c55a5
hasBugs <aaron.salohcin@gmail.com>
2023-01-07 12:19:33 -0600

    renaming/refactoring

--- day4/day4.py
+++ day4/day4.py
@@ -5,21 +5,25 @@ lines = [line.strip().split(',') for line in file.readlines()]
 count = 0
 count2 = 0
 for line in lines:
-    r1 = [int(x) for x in line[0].split('-')] 
-    r2 = [int(x) for x in line[1].split('-')] 
+    s1, e1 = [int(x) for x in line[0].split('-')] 
+    s2, e2 = [int(x) for x in line[1].split('-')] 
 
     
-    if (r1[0] >= r2[0] and r1[1] <= r2[1]) or (r2[0] >= r1[0] and r2[1] <= r1[1]):
+    if (s1 >= s2 and e1 <= e2) or (s2 >= s1 and e2 <= e1):
         count+=1
 
-    if r1[0] >= r2[0] and r1[0] <= r2[1]:
-        count2+=1
-    elif r1[1] >= r2[0] and r1[1] <= r2[1]:
-        count2+=1
-    elif r2[0] >= r1[0] and r2[0] <= r1[1]:
-        count2+=1
-    elif r2[1] >= r1[0] and r2[1] <= r1[1]:
-        count2+=1
+    # if s1 >= s2 and s1 <= e2:
+    #     count2+=1
+    # elif e1 >= s2 and e1 <= e2:
+    #     count2+=1
+    # elif s2 >= s1 and s2 <= e1:
+    #     count2+=1
+    # elif e2 >= s1 and e2 <= e1:
+    #     count2+=1
+
+    # simplified
+    if not (e1 < s2 or e2 < s1):
+        count2 +=1   
 
 print(count)
 print(count2)
