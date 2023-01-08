3bd358961e93e7cd1592295c0a0857ddc17b5db7
hasBugs <aaron.salohcin@gmail.com>
2023-01-07 18:02:18 -0600

    clean up and done

--- day5/day5.py
+++ day5/day5.py
@@ -1,26 +1,40 @@
-file = open("data.txt")
+def getData():
+    file = open("data.txt")
 
-lines = file.readlines()
-stacks = [[] for x in range(9)]
+    lines = file.readlines()
+    stacks = [[] for x in range(9)]
 
-for line in lines[7::-1]:
-    col = 0
-    for num in range(1,35, 4):
-        if(line[num].strip() != ''):
-            stacks[col].append((line[num]))
-        col+=1
+    for line in lines[7::-1]:
+        col = 0
+        for num in range(1,35, 4):
+            if(line[num].strip() != ''):
+                stacks[col].append((line[num]))
+            col+=1
+    return [lines, stacks] 
 
-for i , line in enumerate(lines[10:]):
-    num, frm, to = [int(x.strip()) for x in line.split(' ')[1::2]]
-    
-    for times in range(num):
-        if stacks[frm-1]:
-            stacks[to-1].append(stacks[frm-1].pop())
-    # temp = stacks[frm-1][len(stacks[frm-1])-num:]
-    # stacks[frm-1] = stacks[frm-1][:num]
-    # print(temp)
-    # stacks[to-1].append(stacks[frm-1])
+def part1():
+    lines, stacks = getData()
+    for line in lines[10:]:
+        num, frm, to = [int(x.strip()) for x in line.split(' ')[1::2]]
+        
+        for times in range(num):
+            if stacks[frm-1]:
+                stacks[to-1].append(stacks[frm-1].pop())
+    printTops(stacks)
+
+def part2():
+    lines, stacks = getData()
+    for line in lines[10:]:
+        num, frm, to = [int(x.strip()) for x in line.split(' ')[1::2]]
+
+        temp = stacks[frm-1][len(stacks[frm-1])-num:]
+        stacks[frm-1] = stacks[frm-1][:len(stacks[frm-1])-num] 
+        stacks[to-1] = stacks[to-1] + temp
+    printTops(stacks)
 
 
-print('part1: ' + ''.join([x.pop() for x in stacks]))
-# print('part2: ' + ''.join([x.pop() for x in stacks]))
+def printTops(stacks):
+    print(''.join([x[-1] for x in stacks]))
+
+part1()
+part2()
