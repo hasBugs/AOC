c57cc03448ce2decb2565e60b871f96c286ac209
hasBugs <aaron.salohcin@gmail.com>
2023-10-27 20:43:13 -0500

    idk what that ws

--- day8/day8.py
+++ day8/day8.py
@@ -25,4 +25,25 @@ for r in range(R):
                 vis = True
         if vis:
             p1 += 1
+
+
+p2 = 0
+for r in range(R):
+    for c in range(C):
+        count = 0
+        for (dr,dc) in DIR:
+            rr = r
+            cc = c
+            ok = True
+            while True:
+                rr += dr
+                cc += dc
+                if not (0<=rr<R and 0<=cc<C):
+                    break
+                if lines[rr][cc] >= lines[r][c]:
+                    ok = False
+            if ok:
+                vis = True
+        if vis:
+            p1 += 1            
 print(p1)
